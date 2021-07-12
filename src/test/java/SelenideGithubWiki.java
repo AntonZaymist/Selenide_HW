0ff4e551c200aec094d47ed5b048f41c8bcdd90e
DESKTOP-CDSJHME\fakaf <fakafaka555@gmail.com>
2021-07-12 03:19:46 +0300

    HW 3

---
+++ src/test/java/SelenideGithubWiki.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideGithubWiki {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void checkGitWiki(){
+//        Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+//        Перейдите в раздел Wiki проекта
+        $(byText("Wiki")).click();
+//        Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $(".markdown-body").$(byText("Soft assertions")).click();
+//        Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class")).shouldBe(visible);
+
+
+    }
+}
