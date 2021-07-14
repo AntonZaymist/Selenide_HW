33b53ebc76c88e48d09b9adff28b277b9ca46f45
DESKTOP-CDSJHME\fakaf <fakafaka555@gmail.com>
2021-07-14 17:50:04 +0300

    HW 3.

---
+++ src/test/java/TestDragAndDrop.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestDragAndDrop {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void checkGitWiki(){
+
+//      - Откройте https://the-internet.herokuapp.com/drag_and_drop
+        open("/drag_and_drop");
+//      - Перенесите прямоугольник А на место В
+          SelenideElement col_a = $("#column-a");
+          SelenideElement col_b = $("#column-b");
+//        actions().moveToElement(col_b).click(col_a).build().perform();
+
+        $(col_a).dragAndDropTo(col_b);
+
+//      - Проверьте, что прямоугольники действительно поменялись
+
+        $("#column-a").shouldHave(text("B"));
+
+    }
+}
